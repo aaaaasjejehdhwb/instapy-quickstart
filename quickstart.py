a65cb6d5952d3d64566e3a39289f62bf9fb27d01
aaaaasjejehdhwb <72797975+aaaaasjejehdhwb@users.noreply.github.com>
2020-10-13 13:56:35 +0300

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -3,8 +3,8 @@ from instapy import InstaPy
 from instapy import smart_run
 
 # login credentials
-insta_username = ''
-insta_password = ''
+insta_username = 'abcd'
+insta_password = '1234'
 
 comments = ['Nice shot! @{}',
         'I love your profile! @{}',
